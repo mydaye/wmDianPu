bfade54b469bcf919b6140df7173b2a86ba09fbf
LunKeee <xl755377063@outlook.com>
2025-05-28 17:41:05 +0800

    saveAdmin

--- src/main/java/com/heycolor/wmdianpudemo/mapper/ManageMapper.java
+++ src/main/java/com/heycolor/wmdianpudemo/mapper/ManageMapper.java
@@ -1,8 +1,12 @@
 package com.heycolor.wmdianpudemo.mapper;
 
+import com.heycolor.wmdianpudemo.myBean.admin;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ManageMapper {
 
+    void addAdmin(admin admin);
+
+    void updateAdmin(admin admin);
 }
--- src/main/java/com/heycolor/wmdianpudemo/service/ManageService.java
+++ src/main/java/com/heycolor/wmdianpudemo/service/ManageService.java
@@ -2,13 +2,18 @@ package com.heycolor.wmdianpudemo.service;
 
 
 import com.heycolor.wmdianpudemo.constant.BuzException;
+import com.heycolor.wmdianpudemo.mapper.ManageMapper;
 import com.heycolor.wmdianpudemo.myBean.*;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ManageService {
+
+    @Resource
+    private ManageMapper manageMapper;
 
     public void login(LoginDTO loginDTO) {
         // 参数不能为空
@@ -21,6 +26,26 @@ public class ManageService {
     }
 
     public void saveAdmin(admin admin) {
+        // 参数不能为空
+        if(admin.getApptype() == null || admin.getCode() == null)
+            throw new BuzException("参数不能为空");
+
+        if(admin.getId() == -1){
+            // 新增
+            try {
+                manageMapper.addAdmin(admin);
+            }catch (Exception e){
+                throw new BuzException("新增失败");
+            }
+        }else{
+            //修改
+            try{
+                manageMapper.updateAdmin(admin);
+            }catch (Exception e){
+                throw new BuzException("修改失败");
+            }
+        }
+
     }
 
     public void saveMinfo(minfo minfo) {
